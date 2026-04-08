b44a2d5fb8c40dbe285ade9292057e72f1dfbc92
Raj Gupta <your-email@example.com>
2026-04-09 01:27:12 +0530

    fix email timeout

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ app.post("/mail", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      secure: true,
-      host: "smtp.gmail.com",
-      port: 465,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
